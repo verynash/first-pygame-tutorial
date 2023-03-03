e8e85f057b03326d2567e983900848a7bdbf2042
verynash <sbz.chary@gmail.com>
2023-03-02 23:42:43 -0500

    Add player, enemies, movement, and collisions

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # first-pygame-tutorial
+This is a basic tutorial from https://realpython.com/pygame-a-primer/
+I did not complete this upon first trial because I could not download the image assets, and the ones I found were too large to use. It was also getting late.
+I may complete it in the future, but hopefully this serves as a good jumping off point for the future. I would like to create some games in the future, gotta start somewhere.
---
+++ avoid-game.py
@@ -0,0 +1,130 @@
+import pygame
+import random
+from pygame.locals import (
+    RLEACCEL,
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+pygame.init()
+
+# Define a Player object by extending pygame.sprite.Sprite
+# The surface drawn on the screen is now an attribute of 'player'
+# Instead of a surface, use an image for a better-looking sprite
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.image.load('jet-art.png').convert()
+        self.surf.set_colorkey((255,255,255), RLEACCEL)
+        self.rect = self.surf.get_rect()
+    # Move sprite based on keypresses
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0,-5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5,0)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0,5)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5,0)       
+        # Keep player on the screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.image.load("missile-art.png").convert()
+        self.surf.set_colorkey((255,255,255), RLEACCEL)
+        self.rect = self.surf.get_rect(
+            center = (
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT)
+            )
+        )
+        self.speed = random.randint(5,20)
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
+
+# Define constants for screen H/W
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 700
+
+# Create the screen object - you control the surface and 
+#   OS controls window borders and title bar
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Setup clock for framerate reasons later
+clock = pygame.time.Clock()
+
+# USEREVENT is the last event pygame reserves, so adding 1 ensures its unique
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 500)
+
+# Instantiate player - before this line it is just a rectangle
+player = Player()
+
+# Create groups to hold enemy sprites and all sprites
+# - enemies is used for collision detection and position updates
+# - all_sprites is used for rendering
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+# Variable to keep the main loop running
+running = True
+
+# MAIN LOOP
+while running:
+    # Look at every event in the queue
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+        elif event.type == QUIT:
+            running = False
+        # Add new enemy?
+        elif event.type == ADDENEMY:
+            # Create new enemy and add it to sprite groups
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(new_enemy)
+
+
+    # Get all keys currently pressed
+    pressed_keys = pygame.key.get_pressed()
+
+    # Update player sprite based on user keypresses
+    player.update(pressed_keys)
+    
+    # Update enemies
+    enemies.update()
+
+    screen.fill((135,206,250))
+
+    # Draw sprites
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
+
+    # Check if enemies have collided with the player
+    if pygame.sprite.spritecollideany(player, enemies):
+        player.kill()
+
+    # Update display
+    pygame.display.flip()
+
+    # Ensure 30 fps
+    clock.tick(30)
