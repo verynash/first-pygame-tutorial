9a1a0b77bc082d49c146a68f58b51d4a9d54cb98
verynash <sbz.chary@gmail.com>
2023-03-05 11:11:49 -0500

    Add sounds to movements and collisions

--- avoid-game.py
+++ avoid-game.py
@@ -11,6 +11,7 @@ from pygame.locals import (
     QUIT,
 )
 
+pygame.mixer.init()
 pygame.init()
 
 # Define a Player object by extending pygame.sprite.Sprite
@@ -26,10 +27,12 @@ class Player(pygame.sprite.Sprite):
     def update(self, pressed_keys):
         if pressed_keys[K_UP]:
             self.rect.move_ip(0,-5)
+            move_up_sound.play()
         if pressed_keys[K_LEFT]:
             self.rect.move_ip(-5,0)
         if pressed_keys[K_DOWN]:
             self.rect.move_ip(0,5)
+            move_down_sound.play()
         if pressed_keys[K_RIGHT]:
             self.rect.move_ip(5,0)       
         # Keep player on the screen
@@ -76,6 +79,18 @@ class Cloud(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.kill()
 
+# Load and play background music
+# Sound source: http://ccmixter.org/files/Apoxode/59262
+# License: https://creativecommons.org/licenses/by/3.0/
+pygame.mixer.music.load('Apoxode_-_Electric_1.mp3')
+# Setting loops to -1 makes the loop never end
+pygame.mixer.music.play(loops=-1)
+
+# Load all sound files
+# Sound sources: Jon Fincher
+move_up_sound = pygame.mixer.Sound('Rising_putter.ogg')
+move_down_sound = pygame.mixer.Sound('Falling_putter.ogg')
+collision_sound = pygame.mixer.Sound('Collision.ogg')
 
 # Define constants for screen H/W
 SCREEN_WIDTH = 1200
@@ -149,6 +164,16 @@ while running:
     # Check if enemies have collided with the player
     if pygame.sprite.spritecollideany(player, enemies):
         player.kill()
+        move_up_sound.stop()
+        move_down_sound.stop()
+        collision_sound.play()
+
+        # Stop the music
+        pygame.mixer.music.stop()
+        pygame.mixer.quit()
+
+        # Exit the window
+        running = False
 
     # Update display
     pygame.display.flip()
